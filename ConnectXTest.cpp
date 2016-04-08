b2ccbcf16545ac23981e9c574ba4dc7ae09854ef
chaitanyasponnapalli <cponnapalli@mail.csuchico.edu>
2016-04-08 02:09:36 -0700

    changed ConnectXTest.cpp and added README.md files

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -19,3 +19,133 @@ TEST(ConnectXTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(ConnectXTest, placePieceCheck){
+	ConnectX con(10,5,3);
+	con.placePiece(1);//b
+	con.showBoard();
+	ASSERT_EQ(2,con.at(1,4));//should place black at that position
+}
+TEST(ConnectXTest, turnCheck){
+	ConnectX con(10,5,3);
+	con.placePiece(1);//b
+	ASSERT_EQ(1,con.whoseTurn());//should change the turn to white after placing black	
+}
+//This is a bug, because all the locations which exceed the vector size limit should be set as 'Invalid'
+TEST(ConnectXTest, InvalidNegPiecePlaceCheck){
+	ConnectX con(10,5,3);
+	con.placePiece(-10);//b
+	con.showBoard();
+	ASSERT_EQ(-1,con.at(-9,0));//should return invalid, as placed position is invalid
+	
+}
+//toggle for invalid piece place
+TEST(ConnectXTest, InvalidNegPieceToggleCheck){
+	ConnectX con(10,5,3);
+	con.placePiece(-10);//b
+	ASSERT_EQ(1,con.whoseTurn());//should be white's turn
+}
+
+//This return Invalid because the inbounds sets the flag to false, for the height
+TEST(ConnectXTest, InvalidNegPieceRowCheck){
+	ConnectX con(10,5,3);
+	con.placePiece(-10);//b
+	ASSERT_EQ(-1,con.at(0,-9));//should return invalid, as placed position is invalid
+}
+
+TEST(ConnectXTest, BothOutOfBoundsNegChk){
+	ConnectX con(10,5,3);
+	con.placePiece(1);//b
+	ASSERT_EQ(-1,con.at(-2,-1));//should return invalid location	
+}
+TEST(ConnectXTest, bothInvalidLocCheck){
+	ConnectX con(2,1,3);
+	con.placePiece(1);//b
+	con.showBoard();
+	ASSERT_EQ(-1,con.at(-2,1));	
+}
+TEST(ConnectXTest, outOfBoundsValsCheck){
+	ConnectX con(10,5,3);
+	con.placePiece(1);//b
+	ASSERT_EQ(-1,con.at(12,11));	
+}
+TEST(ConnectXTest, singleInvalidCheck){//should return invalid, but returns a valid location
+	ConnectX con(10,5,3);
+	con.placePiece(1);//b
+	ASSERT_EQ(-1,con.at(12,1));	
+}
+TEST(ConnectXTest, outOfBoundsNegHtChk){
+	ConnectX con(10,5,3);
+	con.placePiece(1);//b
+	ASSERT_EQ(-1,con.at(1,-2));	
+}
+TEST(ConnectXTest, NegHtChk){
+	ConnectX con(10,-3,3);
+	con.placePiece(1);//b
+	ASSERT_EQ(-1,con.at(1,-2));	
+}
+TEST(ConnectXTest, NegWideChk){
+	ConnectX con(-10,3,3);
+	con.placePiece(1);//b
+	ASSERT_EQ(-1,con.at(1,-2));	
+}
+TEST(ConnectXTest, NegToWinChk){
+	ConnectX con(10,3,-3);
+	con.placePiece(1);//b
+	ASSERT_EQ(-1,con.at(1,-2));	
+}
+TEST(ConnectXTest, ToggleTurnCheck){
+	ConnectX con(10,5,3);
+	con.placePiece(1);//b
+	con.placePiece(1);//b
+	con.showBoard();
+	ASSERT_EQ(1,con.at(1,3));	
+}
+TEST(ConnectXTest, heightOverflowCheck){
+	ConnectX con(2,2,2);
+	con.placePiece(1);//b
+	con.placePiece(1);//w
+	/*Invalid locations*/
+	con.placePiece(1);//b
+	ASSERT_EQ(1,con.whoseTurn());
+	con.placePiece(1);
+}
+TEST(ConnectXTest, showBoardToggleCheck){
+	ConnectX con(2,2,2);
+	con.placePiece(1);//b
+	con.showBoard();
+	con.placePiece(1);//b
+	con.showBoard();
+}
+//set to default if invalid dimensions are given
+TEST(ConnectXTest, showBoardNegColCheck){
+	ConnectX con(-1,2,2);
+	con.showBoard();
+}
+/*
+//set to default if invalid dimensions are given. This should give false, but throws double free corruption error
+TEST(ConnectXTest, InvalidExceptionCheck){
+	ConnectX con(5,3,2);	
+	con.placePiece(-1);
+	ASSERT_EQ(0,con.at(1,5));
+}
+*/
+//The pieces should not be placed(as the location is invalid). But, placed at some valid locations
+TEST(ConnectXTest, InvalidPiecePlacement){
+	ConnectX con(10,5,3);
+	con.placePiece(12);//b
+	ASSERT_EQ(2,con.at(0,3));//should be empty, but black is placed at this location
+	con.showBoard();
+}
+
+//Change to default dimensions, if given incorrect dimensions
+TEST(ConnectXTest, defaultDimensionsCheck){
+	ConnectX con(10,-5,3);
+	ASSERT_EQ(6,DEFAULT_HEIGHT);
+}
+
+TEST(ConnectXTest, invalidLocationCheck){
+	ConnectX con(10,5,3);
+	ASSERT_EQ(-1,con.at(20,5));
+}
+
---
+++ README.md
@@ -0,0 +1,24 @@
+Bugs:
+1. In inBounds(int w, int h) method, if the value of height is in bounds(valid value) but width is invalid(out of bounds), the flag(inside) is set to true. This will result in the return value for an invalid location, to be returned as true, indicating the invalid location to be in bounds. This results in incorrect bound checking. 
+The branch condition should be:
+if( (w>=width || w<0) ||   (h<0 || h>=height ) ){
+	return false;
+}
+
+Eg: w = -5, h = 10. When inbounds(-5,10) is called, true is returned. However, these are invalid dimensions.
+
+This is covered in the test case: TEST(ConnectXTest, InvalidNegPiecePlaceCheck). In this testcase, eventhough player places the piece in an invalid column, the piece is still placed in some location. The functionality should be such that, the inbounds should return false and the player just loses his turn.
+
+2. The second bug in the code is when the at(int w, int h) function is called after placing a piece at some valid location. When this function is called for some out of bounds location, instead of returning INVALID, it shows that the location is EMPTY.
+
+Eg: As seen in the testcase TEST(ConnectXTest, singleInvalidCheck), when the piece is placed at some valid location(which is in bounds for dimensions (10,5,3)) after which an invalid location is checked, such as (12,1), instead of showing the location as INVALID, EMPTY is returned for that location.
+
+3. The third bug, which is present in the code, is when a piece is placed at some invalid location(at column -1). After this, when a valid location is checked, then the code breaks. A double free corruption is thrown. The code should just ignore the invalid place request and turn should be toggled. Also, the valid location which is checked, should just return EMPTY.
+
+This is covered in the test case, TEST(ConnectXTest, InvalidExceptionCheck). It is commented out because, the exception breaks the test cases.
+
+4. The fourth bug occurs when a piece is placed at some invalid location, which is out of bounds. This should just ignore the place request with the player losing his turn. However, the piece is placed at some random valid solution. This is incorrect, because the value at this invalid(out of bounds) column is considered as EMPTY, while it is infact supposed to be INVALID.
+
+This is covered in the test case TEST(ConnectXTest, InvalidPiecePlacement).
+
+5. Due to the bug in the inBounds function, the branch condition, at(column,i)==EMPTY as TRUE and inBounds(column,i) as FALSE can never be checked(code coverage for this is 0).
